81554ee460c1751e9c607a3bbd0705ee398458bf
KUMAR CHANDRA BHANU <chbhanu160@gmail.com>
2023-12-30 12:07:04 +0530

    navbar size & link in i tab

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -23,9 +23,10 @@ const Footer = () => {
           <img  src={require('./logo.png')} id="vtm_flogo" alt="vtm" />
             <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
 
-            <i className='fab fa-facebook-f icon'></i>
-            <i className='fab fa-twitter icon'></i>
-            <i className='fab fa-instagram icon'></i>
+            <a href="https://www.linkedin.com/company/visionary-techmate/mycompany/?viewAsMember=true" target="_new"><i className="fab fa-linkedin-in icon" ></i></a>
+            <a href="https://www.facebook.com/profile.php?id=100088829731631" target="_new"><i className='fab fa-facebook-f icon'></i></a>
+            <a href="https://www.instagram.com/cloud__talk/?igshid=OGU0MmVlOWVjOQ%3D%3D" target="_new"> <i className='fab fa-instagram icon'></i></a>
+            <a href="https://www.youtube.com/channel/UC0xHeCcMjsHoPqup193_nNw" target="_new"><i className='fab fa-youtube icon'></i></a>
           </div>
           <div className='box link'>
             <h3>Explore</h3>
--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -16,10 +16,10 @@ const Head = () => {
           </div>
 
           <div id='social'>
-            <i className='fab fa-facebook-f icon'></i>
-            <i className='fab fa-instagram icon'></i>
-            <i className='fab fa-twitter icon'></i>
-            <i className='fab fa-youtube icon'></i>
+            <a href="https://www.linkedin.com/company/visionary-techmate/mycompany/?viewAsMember=true" target="_new"><i className="fab fa-linkedin-in icon" ></i></a>
+            <a href="https://www.facebook.com/profile.php?id=100088829731631" target="_new"><i className='fab fa-facebook-f icon'></i></a>
+            <a href="https://www.instagram.com/cloud__talk/?igshid=OGU0MmVlOWVjOQ%3D%3D" target="_new"> <i className='fab fa-instagram icon'></i></a>
+            <a href="https://www.youtube.com/channel/UC0xHeCcMjsHoPqup193_nNw" target="_new"><i className='fab fa-youtube icon'></i></a>
           </div>
         </div>
       </section>
