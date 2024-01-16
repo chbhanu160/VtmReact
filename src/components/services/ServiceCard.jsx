427538cba1d17545fddc072cd6300f98d49c2618
KUMAR CHANDRA BHANU <chbhanu160@gmail.com>
2024-01-16 17:54:14 +0530

    service section update

---
+++ src/components/services/ServiceCard.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+// import { services } from "../../dummydata"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
+const ServiceCard = ({ name, desc, bulletPoints }) => {
+  return (
+    <div className='services items shadow'>
+      <h4>{name}</h4>
+      <div className="description">
+        {bulletPoints ? (
+          <ul>
+            {bulletPoints.map((point, index) => (
+              <li key={index}>
+                <FontAwesomeIcon icon={faCheckCircle} className="bullet-icon" />
+                {point}
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+      <button className='outline-btn'>GET STARTED</button>
+    </div>
+  );
+};
+
+export default ServiceCard;
