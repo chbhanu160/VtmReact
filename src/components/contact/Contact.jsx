a825801fe648c9cea16ba72e04c57339f6df264e
KUMAR CHANDRA BHANU <chbhanu160@gmail.com>
2024-01-18 21:00:17 +0530

    contact page updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,22 +3,20 @@ import Back from "../common/back/Back";
 import "./contact.css";
 
 const Contact = () => {
-  // const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
   return (
     <>
       {/* <Back title='Contact us' /> */}
       <section className="contacts padding">
         <div className="container_con shadow flexSB">
           <div className="left row">
-            {/* <iframe src={map}></iframe> */}
             <img src={require("./4.png")} alt="contact" />
           </div>
 
-          <div className="right row">
+          <div className="right">
             <h1>Contact us</h1>
-            <p>We're open for any suggestion or just to have a chat</p>
+            <p id="con1">We're open for any suggestion or just to have a chat</p>
 
-            <div className="items grid2">
+            <div className="address-email-phone">
               <div className="box_con address-box">
                 <h4>ADDRESS:</h4>
                 <p>
@@ -29,10 +27,8 @@ const Contact = () => {
               <div className="box_con email-phone-box">
                 <div>
                   <h4>EMAIL:</h4>
-                  <p> visionarytechmate@gmail.com</p>
-                  {/* <p>deeksha.azure@gmail.com</p> */}
-                </div>{" "}
-                <br />
+                  <p>visionarytechmate@gmail.com</p>
+                </div>
                 <div>
                   <h4>PHONE:</h4>
                   <p>+91 6375597272</p>
@@ -52,7 +48,6 @@ const Contact = () => {
                 </div>
                 <div className="form-group">
                   <label htmlFor="contact">Contact No.:</label>
-
                   <div className="flexSB">
                     <select id="countryCode">
                       <option value="+91">+91</option>
@@ -65,10 +60,8 @@ const Contact = () => {
                   <select id="country">
                     <option value="In">India</option>
                   </select>
-                  
                 </div>
               </div>
-              {/* <input type='text' placeholder='Subject' /> */}
               <textarea cols="30" rows="10" placeholder="Message"></textarea>
               <button className="primary-btn">SEND MESSAGE</button>
             </form>
