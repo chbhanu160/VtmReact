ad51e6b93b6cc69120b1382783be26d582bfdea4
KUMAR CHANDRA BHANU <chbhanu160@gmail.com>
2024-01-25 00:05:17 +0530

    all service button modification

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,6 @@
 // Service.jsx
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LearningPage from "./LearningPage";
 import ConsultingPage from "./ConsultingPage";
 
@@ -8,17 +8,8 @@ import ConsultingPage from "./ConsultingPage";
 const Services = () => {
   return (
     <section className="services padding">
-      <Switch>
-        <Route path="/services/learning">
-          <LearningPage />
-        </Route>
-        <Route path="/services/consulting">
-        <div id="head_service" >
-          <h3>Consulting Services</h3>
-        </div>
-          <ConsultingPage />
-        </Route>
-      </Switch>
+        <LearningPage />
+        <ConsultingPage /> 
     </section>
   );
 };
