1353b92cb284c912252204d434040ee359fc6aa2
KUMAR CHANDRA BHANU <chbhanu160@gmail.com>
2024-01-27 02:53:03 +0530

    service page update on home

--- src/components/home/Hservices.jsx
+++ src/components/home/Hservices.jsx
@@ -1,53 +1,157 @@
-import React from "react";
+// Hservices.jsx
+import React, { useState } from "react";
 import Heading from "../common/heading/Heading";
-import "./Hservices.css";
-import { Link } from "react-router-dom";
+import ServiceSlider from "../services/ServiceSlider";
 
 const Hservices = () => {
-  return (
-    <section className="hprice padding">
-      <Heading subtitle="OUR Services" />
-      <div className="Service_container">
-        <div className="category">
-          <h2 className="category-title">Learning Services</h2>
-          <div className="tile">Training Programs</div>
-          <div className="tile">Workshops and Seminars</div>
-          <div className="tile">E-Learning Solutions</div>
-          <div className="tile">Corporate Training</div>
-          <div className="tile">Consulting Services</div>
-          <div className="tile">Learning Management Systems (LMS) </div>
-          <div className="tile">Custom Content Development</div>
-          <div className="tile">Skill Assessments and Testing</div>
-          <div className="tile">Career Development Services</div>
-          <div className="tile">Technology Workforce Solutions</div>
-          <Link to="/services/learning" className="view-all-btn">
-            View All
-          </Link>
-        </div>
+  // Define your learning and consulting services data here
+  const learningServices = [
+    {
+      id: 1,
+      name: "Training Programs:",
+      bulletPoints: ["Technical Training: Courses covering programming languages, software development methodologies, database management, networking, cybersecurity, etc.","Certification Training: Preparation courses for industry-recognized certifications such as CompTIA, Cisco, Microsoft, AWS, etc."],
+    },
+    {
+      id: 2,
+      name: "Workshops and Seminars:",
+      bulletPoints: ["Customized workshops and seminars on specific technologies or industry trends.","Hands-on training sessions to provide practical experience." ],
+    },
+    {
+      id: 3,
+      name: "E-Learning Solutions:",
+      bulletPoints: ["Online courses and modules for remote learning.Interactive e-learning platforms with multimedia content." ],
+    },
+    {
+      id: 4,
+      name: "Corporate Training:",
+      bulletPoints: ["Tailored training programs for businesses to enhance the skills of their employees.","Corporate workshops on topics like project management, Agile methodologies, etc." ],
+    },
+    {
+      id: 5,
+      name: "Consulting Services:",
+      bulletPoints: ["Advisory services to help businesses identify and address their specific IT learning needs. ","Guidance on technology adoption, skill development, and IT strategy." ],
+    },
+    {
+      id: 6,
+      name: "Learning Management Systems (LMS):",
+      bulletPoints: ["Development and implementation of customized learning management systems to track and manage training programs. " ],
+    },
+    {
+      id: 7,
+      name: "Custom Content Development:",
+      bulletPoints: ["Creation of custom training materials, including e-books, videos, simulations, and assessments." ],
+    },
+    {
+      id: 8,
+      name: "Skill Assessments and Testing:",
+      bulletPoints: ["Pre-assessment of skills to identify knowledge gaps.Certification exams and assessments to validate acquired skills." ],
+    },
+    {
+      id: 9,
+      name: "Career Development Services:",
+      bulletPoints: ["Career counseling and guidance for individuals looking to advance in their IT careers.Job placement assistance and resume building workshops." ],
+    },
+    {
+      id: 10,
+      name: "Technology Workforce Solutions: ",
+      bulletPoints: ["Providing skilled IT professionals to organizations on a temporary or permanent basis.IT recruitment services." ],
+    },
 
-        <div className="category">
-          <h2 className="category-title">Consulting Services</h2>
-          <div className="tile">IT Strategy and Planning</div>
-          <div className="tile">Digital Transformation</div>
-          <div className="tile">IT Infrastructure Management</div>
-          <div className="tile">Cybersecurity Consulting</div>
-          <div className="tile">Cloud Computing Services</div>
-          <div className="tile">Enterprise Resource Planning (ERP)</div>
-          <div className="tile">Data Analytics and Business Intelligence</div>
-          <div className="tile">Project Management and Implementation</div>
-          <div className="tile">Software Development and Integration</div>
-          <div className="tile">Business Continuity and Disaster Recovery</div>
-          <div className="tile">IT Governance and Compliance</div>
-          <div className="tile">Vendor Management</div>
-          <div className="tile">Managed IT Services</div>
+  ];
+
+  const consultingServices = [
+    {
+      id: 1,
+      name: "IT Strategy and Planning:",
+      bulletPoints:[ "Developing and aligning IT strategies with overall business objectives."," Creating technology roadmaps for future growth and innovation."]
+    },
+    {
+      id: 2,
+      name: "Digital Transformation:",
+      bulletPoints:[ "Assisting organizations in adopting and integrating digital technologies.","  Implementing solutions for automation, IoT, and other digital initiatives."]
+    },
+    {
+      id: 3,
+      name: "IT Infrastructure Management:",
+      bulletPoints:[ "Assessing and optimizing the existing IT infrastructure.","Planning and implementing upgrades, migrations, or transitions to new technologies."]
+    },
+    {
+      id: 4,
+      name: "Cybersecurity Consulting:",
+      bulletPoints:[ "Assessing and enhancing security postures to protect against cyber threats.","  Developing and implementing cybersecurity policies and protocols."]
+    },
+    {
+      id: 5,
+      name: "Cloud Computing Services:",
+      bulletPoints:[ "Advising on cloud adoption strategies (public, private, or hybrid). "," Migration of applications and data to cloud platforms."," Managing and optimizing cloud resources."]
+    },
+    {
+      id: 6,
+      name: "Enterprise Resource Planning (ERP):",
+      bulletPoints:[ "Implementing and customizing ERP solutions for streamlined business processes."," Upgrading or optimizing existing ERP systems."]
+    },
+    {
+      id: 7,
+      name: "Data Analytics and Business Intelligence:",
+      bulletPoints:[ "Helping organizations derive insights from data.  ","  Implementing analytics tools and platforms."]
+    },
+    {
+      id: 8,
+      name: "Project Management and Implementation:",
+      bulletPoints:[ "Overseeing and managing IT projects from initiation to completion. "," Providing project management methodologies and best practices."]
+    },
+    {
+      id: 9,
+      name: "Software Development and Integration:",
+      bulletPoints:[ "Custom software development to meet specific business needs.","  Integration of third-party applications and systems."]
+    },
+    {
+      id: 10,
+      name: "Business Continuity and Disaster Recovery:",
+      bulletPoints:[ "Designing and implementing plans to ensure business continuity. ","  Developing disaster recovery strategies and solutions."]
+    },
+    {
+      id: 11,
+      name: "IT Governance and Compliance:",
+      bulletPoints:[ "Ensuring compliance with industry regulations and standards."," Establishing governance frameworks for IT operations."]
+    },
+    {
+      id: 12,
+      name: "Vendor Management:",
+      bulletPoints:[ "Evaluating and selecting technology vendors."," Managing relationships with technology suppliers."]
+    },
+    {
+      id: 13,
+      name: "Managed IT Services:",
+      bulletPoints:[ "Offering ongoing IT support, maintenance, and monitoring.  "," Proactive management of IT systems to prevent issues."]
+    },
+  ];
+
+  
+    const [currentService, setCurrentService] = useState("Learning");
+  
+    const handleServiceChange = (serviceName) => {
+      setCurrentService(serviceName);
+    };
+  
+    return (
+      <>
+        <section className="hprice padding">
+          <Heading subtitle="OUR Services" />
+          <div className="service-slider-container">
+            <ServiceSlider
+              services={learningServices}
+              serviceName="Learning"
+            />
+            <ServiceSlider
+              services={consultingServices}
+              serviceName="Consulting"
+            />
+          </div>
       
-          <Link to="/services/consulting" className="view-all-btn">
-            View All
-          </Link>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export default Hservices;
+        </section>
+      </>
+    );
+  };
+  
+  export default Hservices;
