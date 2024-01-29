fb92aea858dc2f0e7d04647a093729ff674f6688
KUMAR CHANDRA BHANU <chbhanu160@gmail.com>
2024-01-30 00:57:35 +0530

    update error in carousel

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,10 +1,8 @@
 // Gallery.jsx
-
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 import "./gallery.css";
-import { Carousel } from 'react-bootstrap'; // Import Carousel from react-bootstrap
 
-// Import your image files
 import image1 from './Training/1.jpeg';
 import image2 from './Training/2.jpeg';
 import image3 from './Training/3.jpeg';
@@ -18,12 +16,12 @@ import image10 from './Training/10.jpeg';
 import image11 from './Training/11.jpeg';
 
 const Gallery = () => {
-  // Array of imported image files
-  const galleryImages = [image1, image2, image3, image4, image5, image6, image7,image8,image9,image10,image11];
+  const galleryImages = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11];
 
   return (
-    <div className="gallery">
-      <Carousel className="carousel gallery-carousel">
+    <div className="gallery-container">
+  
+      <Carousel className="gallery-carousel">
         {galleryImages.map((image, index) => (
           <Carousel.Item key={index}>
             <img className="d-block w-100" src={image} alt={`Slide ${index + 1}`} />
