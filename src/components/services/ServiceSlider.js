4d39e004325f593d03e6c3d7c92f59063bd83f57
KUMAR CHANDRA BHANU <chbhanu160@gmail.com>
2024-02-02 23:40:19 +0530

    Hservice update

--- src/components/services/ServiceSlider.js
+++ src/components/services/ServiceSlider.js
@@ -1,4 +1,3 @@
-// ServiceSlider.jsx
 import React, { useState } from "react";
 import ServiceCard from "./ServiceCard";
 import "./ServiceSlider.css";
@@ -7,34 +6,32 @@ const ServiceSlider = ({ services, serviceName }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const handleNextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide + 1) % services.length);
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % (services.length - 2));
   };
 
   const handlePrevSlide = () => {
     setCurrentSlide(
-      (prevSlide) => (prevSlide - 1 + services.length) % services.length
+      (prevSlide) => (prevSlide - 1 + (services.length - 2)) % (services.length - 2)
     );
   };
 
   return (
     <div className="service-slider-container">
-
-<div className="service-slider">
-      <h3  >{serviceName} Services</h3>
-      <div className="slider-content">
-        <div
-          id={`slide ${currentSlide === 0 ? "active" : ""}`}
-        >
-          <ServiceCard {...services[currentSlide]} />
+      <div className="service-slider">
+        <h3>{serviceName} Services</h3>
+        <div className="slider-content">
+          {services.slice(currentSlide, currentSlide + 3).map((service, index) => (
+            <div key={index} className="slide">
+              <ServiceCard {...service} />
+            </div>
+          ))}
+        </div>
+        <div className="slider-buttons">
+          <button onClick={handlePrevSlide}>&#8249; Prev</button>
+          <button onClick={handleNextSlide}>Next &#8250;</button>
         </div>
       </div>
-      <div className="slider-buttons">
-        <button onClick={handlePrevSlide}>&#8249; Prev</button>
-        <button onClick={handleNextSlide}>Next &#8250;</button>
-      </div>
     </div>
-    </div>
-   
   );
 };
 
