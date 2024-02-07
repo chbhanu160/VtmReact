ed604c8c031cb805cf517271ab17ec52a14b6eb2
KUMAR CHANDRA BHANU <chbhanu160@gmail.com>
2024-02-07 21:14:10 +0530

    backgrond image on home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,18 +5,21 @@ import HAbout from "./HAbout";
 import Hero from "./hero/Hero";
 import Hservices from "./Hservices";
 import Testimonal from "./testimonal/Testimonal";
-
+import "./home.css"
 
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <div className="bg-home">
       <Hservices />
       <AboutCard />
       <HAbout />
       <Testimonal />
       <Hblog />
+      </div>
+      
     </>
   );
 };
