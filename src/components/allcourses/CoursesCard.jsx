f3e50832f81fe78f90580b72608093bfe2a9baa9
KUMAR CHANDRA BHANU <chbhanu160@gmail.com>
2024-02-11 01:53:41 +0530

    course section update

--- src/components/allcourses/CoursesCard.jsx
+++ src/components/allcourses/CoursesCard.jsx
@@ -1,59 +1,19 @@
-import React from "react"
-import "./courses.css"
-import { coursesCard } from "../../dummydata"
+// CourseCard.jsx
+import React from 'react';
+import './courses.css';
 
-const CoursesCard = () => {
+const CourseCard = ({ icon, title, hours, description }) => {
   return (
-    <>
-      <section className='coursesCard'>
-        <div className='container grid2'>
-          {coursesCard.map((val) => (
-            <div className='items'>
-              <div className='content flex'>
-                <div className='left'>
-                  <div className='img'>
-                    <img src={val.cover} alt='' />
-                  </div>
-                </div>
-                <div className='text'>
-                  <h1>{val.coursesName}</h1>
-                  <div className='rate'>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <label htmlFor=''>(5.0)</label>
-                  </div>
-                  <div className='details'>
-                    {val.courTeacher.map((details) => (
-                      <>
-                        <div className='box'>
-                          <div className='dimg'>
-                            <img src={details.dcover} alt='' />
-                          </div>
-                          <div className='para'>
-                            <h4>{details.name}</h4>
-                          </div>
-                        </div>
-                        <span>{details.totalTime}</span>
-                      </>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className='price'>
-                <h3>
-                  {val.priceAll} / {val.pricePer}
-                </h3>
-              </div>
-              <button className='outline-btn'>ENROLL NOW !</button>
-            </div>
-          ))}
-        </div>
-      </section>
-    </>
-  )
-}
+    <div className="course-card">
+      <div className="course-icon">{icon}</div>
+      <div className="course-details">
+        <h3>{title}</h3>
+        <p>{hours} Hours</p>
+        <p>{description}</p>
+        <button className="know-more-button">Know More</button>
+      </div>
+    </div>
+  );
+};
 
-export default CoursesCard
+export default CourseCard;
