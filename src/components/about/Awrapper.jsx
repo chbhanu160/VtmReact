1f58093e21cb68f9780575059f22469410e12eff
aniketraj1615 <aniketraj1615@gmail.com>
2024-03-24 14:12:01 +0530

    modification in awrapper done

--- src/components/about/Awrapper.jsx
+++ src/components/about/Awrapper.jsx
@@ -15,10 +15,12 @@ library.add(fab, fas);
 const Awrapper = () => {
   return (
     <>
+    
       <section className='awrapper'>
         <div className='container grid'>
           {awrapper.map((val, index) => {
             return (
+              <div className="d-flex justify-content-center align-items-center">
               <div className='box flex' key={index}>
                 <div className='aw_img'>
                 {val.cover && <FontAwesomeIcon icon={val.cover} />}
@@ -28,6 +30,7 @@ const Awrapper = () => {
                   <h1><NumberCounter endValue={val.data} /></h1>
                   <h3 id="title_text">{val.title}</h3>
                 </div>
+              </div>
               </div>
             );
           })}
--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -15,7 +15,7 @@ const Head = () => {
           <div id='social'>
             <a href="https://www.linkedin.com/company/visionary-techmate/mycompany/?viewAsMember=true" target="_new"><i className="fab fa-linkedin-in icon" ></i></a>
             <a href="https://www.facebook.com/profile.php?id=100088829731631" target="_new"><i className='fab fa-facebook-f icon'></i></a>
-            <a href="https://www.instagram.com/cloud__talk/?igshid=OGU0MmVlOWVjOQ%3D%3D" target="_new"> <i className='fab fa-instagram icon'></i></a>
+            <a href="https://www.instagram.com/cloud__talk/?igshid=OGU0MmVlOWVjOQ%3D%3D" target="_new"><i className='fab fa-instagram icon'></i></a>
             <a href="https://www.youtube.com/channel/UC0xHeCcMjsHoPqup193_nNw" target="_new"><i className='fab fa-youtube icon'></i></a>
           </div>
         </div>
