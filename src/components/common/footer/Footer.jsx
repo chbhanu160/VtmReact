9bdf84c27d8a51a5b9af474cfe2ffe6b97bc30e9
aniketraj1615 <aniketraj1615@gmail.com>
2024-05-12 12:45:35 +0530

    test

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -10,34 +10,34 @@ const Footer = () => {
         <div className="container padding">
           <div className="box logo">
             <img src={logo} id="vtm_flogo" alt="vtm" />
-            <div id="f-social">
+            <div>
               <a
                 href="https://www.linkedin.com/company/visionary-techmate/mycompany/?viewAsMember=true"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <i className="fab fa-linkedin-in icon"></i>
+                <i className="fab fa-linkedin-in icon s_media_icon"></i>
               </a>
               <a
                 href="https://www.facebook.com/profile.php?id=100088829731631"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <i className="fab fa-facebook-f icon"></i>
+                <i className="fab fa-facebook-f icon s_media_icon"></i>
               </a>
               <a
                 href="https://www.instagram.com/cloud__talk/?igshid=OGU0MmVlOWVjOQ%3D%3D"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <i className="fab fa-instagram icon"></i>
+                <i className="fab fa-instagram icon s_media_icon"></i>
               </a>
               <a
                 href="https://www.youtube.com/channel/UC0xHeCcMjsHoPqup193_nNw"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <i className="fab fa-youtube icon"></i>
+                <i className="fab fa-youtube icon s_media_icon"></i>
               </a>
             </div>
           </div>
