dc7459bd9993378f8d9f60d08031d99129ec614a
keshavjha005 <jhakeshavkumar847225@gmail.com>
2024-05-16 12:46:54 +0530

    header update

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,11 +1,11 @@
 // Header.js
+
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Head from "./Head";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./header.css";
-
 
 const Header = () => {
   const [click, setClick] = useState(false);
@@ -14,39 +14,57 @@ const Header = () => {
 
   return (
     <>
-     <Head/>
-      <div >
-        <Navbar id="navbg" expand="lg">
-          <Navbar.Toggle onClick={handleClick} aria-controls="navbarT" />
-          <Navbar.Collapse id="navbar">
-            <Nav id="navbar-item">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/courses">Courses</Nav.Link>
-              <Nav.Link as={Link} to="/about">About</Nav.Link>
+      <Head /> {/* Move the Head component outside of the Navbar */}
+      <Navbar id="navbg" expand="lg">
+        <Navbar.Toggle onClick={handleClick} aria-controls="navbarT" />
+        <Navbar.Collapse id="navbar">
+          <Nav id="navbar-item">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/courses">
+              Courses
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              About
+            </Nav.Link>
 
-              <NavDropdown title="Services" classname="dropdown">
-                <NavDropdown.Item as={Link} to="/services">All Services</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item as={Link} to="/services/learning">Learning</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/services/consulting">Consulting</NavDropdown.Item>
-              </NavDropdown>
+            <NavDropdown title="Services" classname="dropdown">
+              <NavDropdown.Item as={Link} to="/services">
+                All Services
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to="/services/learning">
+                Learning
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/services/consulting">
+                Consulting
+              </NavDropdown.Item>
+            </NavDropdown>
 
-              <Nav.Link as={Link} to="/testimonials">Testimonials</Nav.Link>
-              <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
-              <Nav.Link as={Link} to="/events">Events</Nav.Link>
-              <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
-              <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
-            </Nav>
-            <div className="start">
-              
-              <Link to="/courses" className="Gc_button">
+            <Nav.Link as={Link} to="/testimonials">
+              Testimonials
+            </Nav.Link>
+            <Nav.Link as={Link} to="/gallery">
+              Gallery
+            </Nav.Link>
+            <Nav.Link as={Link} to="/events">
+              Events
+            </Nav.Link>
+            <Nav.Link as={Link} to="/blog">
+              Blog
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              Contact Us
+            </Nav.Link>
+          </Nav>
+          <div className="start">
+            <Link to="/courses" className="Gc_button">
               GET CERTIFICATE
-             </Link>
-             </div>
-
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
+            </Link>
+          </div>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   );
 };
