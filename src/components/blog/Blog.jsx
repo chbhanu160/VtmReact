f85e8a65d1aa599c0bf3916f6f30f9faf5ab9420
aniketraj1615 <aniketraj1615@gmail.com>
2024-05-20 12:50:28 +0530

    push by aniket

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -5,7 +5,6 @@ import "./blog.css"
 const Blog = () => {
   return (
     <>
-      <Back title='Blog Posts' />
       <section className='blog padding'>
         <div className='container grid2'>
           <BlogCard />
