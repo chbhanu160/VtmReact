305947b68131f6ff2f4bd8936efd612016450adb
aniketraj1615 <aniketraj1615@gmail.com>
2024-05-22 14:47:36 +0530

    cosulting services modified by aniket

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -21,12 +21,15 @@ const Hero = () => {
             <p >Embark on a journey into the Future of Tech Excellence with Visionary Techmate, where we transcend boundaries to seamlessly deliver cutting-edge knowledge through our comprehensive range of cloud-based courses and services. Elevate your skills and empower your career with the flexibility and innovation that define our digital learning experience. Join a community dedicated to pushing the boundaries of technology, fostering creativity, and shaping the next generation of tech leaders. At Visionary Techmate, we don't just teach tech, we shape futures.</p>
             </div>
             <Link to="/services/learning" >
-            <Button className="mx-5 my-5" variant="primary">GET STARTED NOW <i className="fa fa-long-arrow-alt-right"></i></Button>{' '}
+            <Button className="mx-5 my-5" variant="primary">GET STARTED NOW<i className="fa fa-long-arrow-alt-right"></i></Button>
             </Link>
             
             <Link to="/courses">
-            <Button variant="primary mx-5 my-5">VIEW COURSE <i className="fa fa-long-arrow-alt-right"></i></Button>{' '}
-            </Link>
+  <Button className="primary mx-5 my-5">
+    VIEW COURSE<i className="fa fa-long-arrow-alt-right"></i>
+  </Button>
+</Link>
+
             
           </div>
           <div className="col-lg-7 col-md-6 col-sm-12" id="image_col" >
