6889ce14763e8894c710079ee6851287ccbc9f67
aniketraj1615 <aniketraj1615@gmail.com>
2024-05-31 15:45:41 +0530

    contact us is fixed by aniket

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import emailjs from 'emailjs-com';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./contact.css";
 
 const Contact = () => {
@@ -12,14 +13,10 @@ const Contact = () => {
       .then((result) => {
         console.log(result.text);
         console.log('message sent');
-
-        // Set showPopup to true after successful submission
         setShowPopup(true);
-
-        // Close the popup and clear the form after 3 seconds
         setTimeout(() => {
           setShowPopup(false);
-          e.target.reset(); // Reset the form fields
+          e.target.reset();
         }, 6000);
       })
       .catch((error) => {
@@ -27,117 +24,83 @@ const Contact = () => {
       });
   };
 
-
   return (
     <>
       <section>
-        <div className="container_con shadow flexSB">
-          <div className="left row">
-            <img src={require("./15.svg")} alt="contact" />
-          </div>
-
-          <div className="right">
-            <div>
-              <h3>Contact Us</h3>
-              <div>
-                <p>We're open for any suggestion or just to have a chat</p>
-              </div>
-              <div className="row w-100">
-                <div className="col">
-                  <div className="d-flex justify-content-center">
-                    <div>
-                      <i className="fa fa-envelope"></i>
-                    </div>
-                    <div className="mx-3">
-                      <p>visionarytechmate@gmail.com</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col">
-                  <div className="d-flex justify-content-center">
-                    <div>
-                      <i className="fa fa-phone-alt"></i>
-                    </div>
-                    <div className="mx-3">
-                      <p>+91 6375597272</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row mx-0">
-                <div className="d-flex justify-content-center">
-                  <div>
-                    <i className="fa fa-location-arrow"></i>
-                  </div>
-                  <div>
-                    <p>Sahkar Nagar, Khatipura road, jhotwara, Jaipur, Rajasthan - 302012</p>
-                  </div>
-                </div>
-              </div>
-
+        <div className="container shadow p-4 my-4">
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-sm-12" id="id_for_img">
+              {/* <img src={require("./15.svg")} alt="contact" className="img-fluid" /> */}
             </div>
 
-            <form id="contactForm" onSubmit={sendEmail}>
-  <div className="d-flex flex-column">
-    <div className="row w-100">
-      <div className="col">
-        <input type="text" className="form-control" placeholder="Name" name="from_name" required />
-      </div>
-      <div className="col">
-        <input type="email" className="form-control" placeholder="Email" name="from_mail" required />
-      </div>
-    </div>
-    <div className="row w-100">
-      <div className="col">
-        <div className="input-group">
-          <select id="countryCode" className="form-control" name="from_country" required>
-            <option value="+91">+91</option>
-            {/* Add more options if needed */}
-          </select>
-          <input type="tel" id="contact" className="form-control contact-input w-50" placeholder="Contact No." name="from_contact" required />
-        </div>
-      </div>
-      <div className="col">
-        <select id="country" className="form-control" name="from_country" required>
-          <option value="Country">Country</option>
-          <option value="India">India</option>
-          <option value="United States">United States</option>
-          {/* Add more options if needed */}
-        </select>
-      </div>
-    </div>
-                <div className="row w-100">
-                  <div className="form-group">
-                    <textarea cols="8" rows="3" className="form-control" placeholder="Message" name="message"></textarea>
-                    <button type="submit" className="primary-btn">SEND MESSAGE</button>
+            <div className="col-lg-6 col-md-6 col-sm-12" id="right_col">
+              <h3>Contact Us</h3>
+              <p>We're open for any suggestion or just to have a chat</p>
+
+              <div className="mb-3">
+                <p><i className="fa fa-envelope"></i> visionarytechmate@gmail.com</p>
+                <p><i className="fa fa-phone-alt"></i> +91 6375597272</p>
+                <p><i className="fa fa-location-arrow"></i> Sahkar Nagar, Khatipura road, jhotwara, Jaipur, Rajasthan - 302012</p>
+              </div>
+
+              <form id="contactForm" onSubmit={sendEmail}>
+                <div className="mb-3">
+                  <div className="row">
+                    <div className="col-md-6 mb-3 mb-md-0">
+                      <input type="text" className="form-control" placeholder="Name" name="from_name" required />
+                    </div>
+                    <div className="col-md-6">
+                      <input type="email" className="form-control" placeholder="Email" name="from_mail" required />
+                    </div>
                   </div>
                 </div>
+                <div className="mb-3">
+                  <div className="row">
+                    <div className="col-md-6 mb-3 mb-md-0">
+                      <div className="input-group">
+                        <select id="countryCode" className="form-control" name="from_country" required>
+                          <option value="+91">+91</option>
+                        </select>
+                        <input type="tel" id="contact" className="form-control w-50" placeholder="Contact No." name="from_contact" required />
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <select id="country" className="form-control" name="from_country" required>
+                        <option value="Country">Country</option>
+                        <option value="India">India</option>
+                        <option value="United States">United States</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <textarea cols="8" rows="3" className="form-control" placeholder="Message" name="message"></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">SEND MESSAGE</button>
+              </form>
+
+              {showPopup && (
+                <div className="alert alert-success mt-3">
+                  <p>Your message has been received successfully. We appreciate your inquiry and will get back to you shortly.</p>
+                  <button className="btn btn-secondary" onClick={() => setShowPopup(false)}>Close</button>
+                </div>
+              )}
+
+              <h3 className="mt-4">Follow us here</h3>
+              <div id="c-social" className="d-flex justify-content-center">
+                <a href="https://www.linkedin.com/company/visionary-techmate/mycompany/?viewAsMember=true" target="_new">
+                  <i className="fab fa-linkedin-in icon"></i>
+                </a>
+                <a href="https://www.facebook.com/profile.php?id=100088829731631" target="_new">
+                  <i className="fab fa-facebook-f icon"></i>
+                </a>
+                <a href="https://www.instagram.com/cloud__talk/?igshid=OGU0MmVlOWVjOQ%3D%3D" target="_new">
+                  <i className="fab fa-instagram icon"></i>
+                </a>
+                <a href="https://www.youtube.com/channel/UC0xHeCcMjsHoPqup193_nNw" target="_new">
+                  <i className="fab fa-youtube icon"></i>
+                </a>
               </div>
-            </form>
-
-            {showPopup && (
-              <div className="popup">
-                <p>Your message has been received successfully. We appreciate your inquiry and will get back to you shortly.</p>
-                <button onClick={() => setShowPopup(false)}>Close</button>
-              </div>
-            )}
-
-
-            <h3>Follow us here</h3>
-
-            <div id="c-social" className="d-flex justify-content-center">
-              <a href="https://www.linkedin.com/company/visionary-techmate/mycompany/?viewAsMember=true" target="_new">
-                <i className="fab fa-linkedin-in icon"></i>
-              </a>
-              <a href="https://www.facebook.com/profile.php?id=100088829731631" target="_new">
-                <i className="fab fa-facebook-f icon"></i>
-              </a>
-              <a href="https://www.instagram.com/cloud__talk/?igshid=OGU0MmVlOWVjOQ%3D%3D" target="_new">
-                <i className="fab fa-instagram icon"></i>
-              </a>
-              <a href="https://www.youtube.com/channel/UC0xHeCcMjsHoPqup193_nNw" target="_new">
-                <i className="fab fa-youtube icon"></i>
-              </a>
             </div>
           </div>
         </div>
