b3badb73c1e64c2aaf9d8df9117c465f1005908e
keshavjha005 <jhakeshavkumar847225@gmail.com>
2024-06-18 15:24:33 +0530

    blog page update and responsive

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,17 +1,15 @@
-import React from "react"
-import BlogCard from "./BlogCard"
-import "./blog.css"
+import React from "react";
+import BlogCard from "./BlogCard";
+import "./blog.css";
 
 const Blog = () => {
   return (
-    <>
-      <section className='blog padding'>
-        <div className='container grid2'>
-          <BlogCard />
-        </div>
-      </section>
-    </>
-  )
-}
+    <section className="blog padding">
+      <div className="blog_container grid2">
+        <BlogCard />
+      </div>
+    </section>
+  );
+};
 
-export default Blog
+export default Blog;
--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import { blog } from "../../dummydata";
+import "./blog.css";
 
 const BlogCard = () => {
   return (
     <>
       {blog.map((val) => (
-        <div className='items shadow' key={val.id}>
-          <div className='blog_img'>
-            <img src={val.cover} alt='Blog Cover' />
+        <div className="blog_item shadow" key={val.id}>
+          <div className="blog_img">
+            <img src={val.cover} alt="Blog Cover" />
           </div>
-          <div className='text'>
-            <div className='admin flexSB'>
+          <div className="blog_text">
+            <div className="blog_admin flexSB">
               <span>
-                <i className='fa fa-user'></i>
-                <label htmlFor=''>{val.type}</label>
+                <i className="fa fa-user"></i>
+                <label>{val.type}</label>
               </span>
               <span>
-                <i className='fa fa-calendar-alt'></i>
-                <label htmlFor=''>{val.date}</label>
+                <i className="fa fa-calendar-alt"></i>
+                <label>{val.date}</label>
               </span>
             </div>
             <h1>{val.title}</h1>
             <p>{val.desc}</p>
           </div>
-
-          <div>
+          <div className="blog_button_container">
             <button onClick={() => window.open(val.to, "_blank")} type="button">Read More</button>
           </div>
         </div>
