516aa6ff1a5128ec3b60dfd3e572eb2d676f0f98
keshavjha005 <jhakeshavkumar847225@gmail.com>
2024-06-20 15:33:10 +0530

    head social media icon update

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -1,35 +1,46 @@
 import React from "react";
+import "./header.css";
 
 const Head = () => {
   return (
-    
-      <section className='head'>
-        <div className='container'>
-          <div className='row flexSB'>
-          <div className="col-lg-10 col-md-6 col-sm-12 d-flex justify-content-start" id="vtm_hlogo">
-  <img src={require('./logo.png')} alt="vtm" className="img-fluid" />
-</div>
-
-
-            <div className="col-lg-2 col-md-6 col-sm-12" id='h-social'>
-              <a href="https://www.linkedin.com/company/visionary-techmate/mycompany/?viewAsMember=true" target="_new">
-                <i className="fab fa-linkedin-in icon"></i>
-              </a>
-              <a href="https://www.facebook.com/profile.php?id=100088829731631" target="_new">
-                <i className='fab fa-facebook-f icon'></i>
-              </a>
-              <a href="https://www.instagram.com/cloud__talk/?igshid=OGU0MmVlOWVjOQ%3D%3D" target="_new">
-                <i className='fab fa-instagram icon'></i>
-              </a>
-              <a href="https://www.youtube.com/channel/UC0xHeCcMjsHoPqup193_nNw" target="_new">
-                <i className='fab fa-youtube icon'></i>
-              </a>
-            </div>
-          </div>
+    <section className='head'>
+      <div className='head_container'>
+        <div className='logo'>
+          <img src={require('./logo.png')} alt="vtm" className="img-fluid" />
         </div>
-      </section>
-    
-  )
+        <div className='social_icons'>
+          <a
+            href="https://www.linkedin.com/company/visionary-techmate/mycompany/?viewAsMember=true"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-linkedin-in icon s_media_icon"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=100088829731631"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-facebook-f icon s_media_icon"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/cloud__talk/?igshid=OGU0MmVlOWVjOQ%3D%3D"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-instagram icon s_media_icon"></i>
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UC0xHeCcMjsHoPqup193_nNw"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-youtube icon s_media_icon"></i>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Head;
